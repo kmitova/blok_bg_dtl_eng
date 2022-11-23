411250dbebfa8ec58d0b67e3aa17d1cf385dd3d3
kmitova <96522051+kmitova@users.noreply.github.com>
2022-11-23 15:02:42 +0200

    private header and home app

--- blok_bg_dtl/settings.py
+++ blok_bg_dtl/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'common',
     'accounts',
+    'home',
 ]
 
 MIDDLEWARE = [
--- blok_bg_dtl/urls.py
+++ blok_bg_dtl/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
     path('accounts/', include('accounts.urls')),
+    path('home/', include('home.urls')),
 ]
 
 
