d699eb150d9d8d6a4cda5e9f53f4637ac5f64d86
kmitova <96522051+kmitova@users.noreply.github.com>
2022-11-24 11:13:49 +0200

    added profile menu

--- staticfiles/scripts/app.js
+++ staticfiles/scripts/app.js
@@ -1,1 +1,11 @@
+console.log('test4')
 console.log('test')
+
+console.log('test2')
+// profile menu
+console.log(document.getElementById('profile-icon'))
+document.getElementById('profile-icon').
+    addEventListener('click', () => {
+        console.log('from func')
+        document.getElementById('profile-choices').style.display = 'block'
+    })
