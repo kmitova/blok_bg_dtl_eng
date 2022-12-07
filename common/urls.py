56d2ae0f5189356985bd2fce33a4c7751831f2ea
kmitova <96522051+kmitova@users.noreply.github.com>
2022-12-07 17:36:40 +0200

    footer items

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from common.views import landing_page
+from common.views import landing_page, about_page, t_and_c_page, privacy_page
 
 urlpatterns = (
     path('', landing_page, name='landing page'),
+    path('about/', about_page, name='about page'),
+    path('terms&conditions/', t_and_c_page, name='t-and-c page'),
+    path('privacy-and-data-protection/', privacy_page, name='privacy page'),
 )
--- common/views.py
+++ common/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'common/landing-page.html')
+
+
+def about_page(request):
+    return render(request, 'common/about.html')
+
+
+def t_and_c_page(request):
+    return render(request, 'common/t-and-c.html')
+
+def privacy_page(request):
+    return render(request, 'common/privacy.html')
